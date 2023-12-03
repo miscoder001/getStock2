39a0a26ec7ee63303b922f6f13d612e030ebe039
Liang <miscoder001@gmail.com>
2023-12-03 11:55:45 +0800

    getStock 初始版本

---
+++ main.py
@@ -0,0 +1,7 @@
+# F12 selector 推薦
+# span.IsqQVc.NprOob.wT3VGc
+# 載入必要套件
+from pyquery import  PyQuery as pq
+
+#發出 request 載入網頁
+page = pq(url="https://www.google.com/search?q=2345",encoding="utf-8", header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
